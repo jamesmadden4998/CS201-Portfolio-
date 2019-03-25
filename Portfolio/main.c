f11dabc7d8baa99eb2a34956478dd7c946ebae17
James Madden <james.madden4998@gmail.com>
2019-03-25 18:21:01 -0500

    Set a number oriented menu

--- Portfolio/main.c
+++ Portfolio/main.c
@@ -6,74 +6,89 @@
 #include "blankAVL.h"
 #include "readIn.h"
 
-char command[50];
-char buffer[150];
-char lookup[150];
+int command;
 
 void promptInitial(){
-    printf("Enter the name of the list you wish to create or edit:\n");
-    printf("Enter 'q' to exit program\n");
+    printf("Enter (1) to View all list titles.\n");
+    printf("Enter (2) to Create or Edit a list.\n");
+    printf("Enter (3) to Delete a list.\n");
+    printf("Enter (9) to Exit program\n");
     printf("->");
-    scanf(" %[^\n]s", buffer);
-    printf("You entered: %s\n\n", buffer);
+    scanf(" %d", &command);
 }
 
 void promptSecond(){
-    printf("To view the contents of this list enter 'print'\n");
-    printf("To search for a title to add to the list enter'add'\n");
-    printf("Enter 'q' to exit program\n");
+    printf("Enter (1) to View all list titles.\n");
+    printf("Enter (4) To View the contents of this list.\n");
+    printf("Enter (5) to Add to this list.\n");
+    printf("Enter (6) to Delete a movie from this.\n");
+    printf("Enter (7) to Select a different list.\n");
+    printf("Enter (9) to exit program.\n");
     printf("->");
-    scanf(" %[^\n]s", command);
+    scanf(" %d", &command);
 }
 
 void resetCommand(){
-    strcpy(command, "none");
+    command = -1;
 }
 
 int main(){
-
-
 
     Node *root;
     root = readIn();
 
     int count = 0;
-    char arrayoflists[count][100];
+    char arrayoflists[100][100];
+    char listTitle[150];
+    char lookup[150];
 
-    while(strcmp(command, "q") != 0){
-        if(count == 0){
+    char inLine[128];
+    FILE *ListN = fopen("ListNames", "r");
+    while(fgets(inLine, sizeof(inLine), ListN) != NULL){
+        strcpy(arrayoflists[count], inLine);
+        count++;
+    }
+    fclose(ListN);
+
+    while(command != 9){
+
+        if(command == 7 || command == 0){
+            resetCommand();
             promptInitial();
-            strcpy(arrayoflists[count], buffer);
-            count++;
-            promptSecond();
         }
-        else if(strcmp(command,"select") == 0){
-            promptInitial();
-            // int i = 0;
+
+        else if(command == 2){
+            char errand;
+            printf("Enter the title of the list you wish ot Create or Edit.\n");
+            printf("->");
+            scanf(" %[^\n]s", listTitle);
+            errand = getchar();
             int key = 0;
             int i = 0;
             char *listname = malloc(100 * sizeof(char));
             char *compare = malloc(100 * sizeof(char));
-            listname = LOW(buffer);
+            strcpy(listname, LOW(listTitle));
             while(arrayoflists[i] != NULL){
                 //printf("In while...\n");
-                compare = LOW(arrayoflists[i]);
+                strcpy(compare, LOW(arrayoflists[i]));
                 if(strcmp(listname,compare)==0){
                     printf("The List Exists\n");
                     key = 34;
                     break;
                 }
-                else if(i >= count)break;
+                if(i > count)break;
                 i++;
             }
+            i--;
             if (key != 34){
+                printf("I...Count is    %d...%d\n", i, count);
+                char ch;
                 printf("The list does not exist yet. Would you like to create it? (Y/N)\n");
                 printf("->");
-                char c;
-                scanf(" %c", &c);
-                if((c == 'Y')||(c == 'y')){
+                scanf("%c", &ch);
+                if((ch == 'Y')||(ch == 'y')){
                     count++;
-                    strcpy(arrayoflists[i], buffer);
+                    strcpy(arrayoflists[i], listTitle);
                     FILE *fp = fopen("ListNames", "w");
                     int h = 0;
                     for(h; h<count; h++){
@@ -85,9 +100,9 @@ int main(){
             resetCommand();
             promptSecond();
         }
-        else if(strcmp(command,"print") == 0){
+        else if(command == 4){
             char line[128];
-            FILE *fptr = fopen(buffer, "r");
+            FILE *fptr = fopen(listTitle, "r");
             if(fptr == NULL){
                 printf("list is empty\n");
                 fclose(fptr);
@@ -99,21 +114,21 @@ int main(){
                 }
                 fclose(fptr);
             }
-            resetCommand();
             promptSecond();
         }
-        else if(strcmp(command, "add") == 0){
-            printf("Enter the title you want to search for:\n");
+        else if(command == 5){
+            printf("Enter the title you want to search for:\n->");
             scanf(" %[^\n]s", lookup);
             printf("You entered: %s\n", lookup);
             Node *found = searchFor(root, lookup);
             if(found != NULL){
-                printf("Are you you sure you want to add this to your list? (Y/N)\n\n");
                 printNode(found);
+                printf("Are you you sure you want to add this to your list? (Y/N)\n\n");
+                printf("->");
                 char c;
                 scanf(" %c", &c);
                 if(c == 'Y' || c == 'y'){
-                    FILE *fp = fopen(buffer, "a");
+                    FILE *fp = fopen(listTitle, "a");
                     printNodeFile(fp, found);
                     fclose(fp);
                 }
@@ -129,13 +144,18 @@ int main(){
             resetCommand();
             promptSecond();
         }
-        else if(strcmp(command, "pList") == 0){
+        else if(command == 1){
             int r = 0;
-            for(r; r<count; r++){
-                printf("%s\n", arrayoflists[r]);
+            if(count == 0){
+                printf("There currently are no made lists.\n");
+            }
+            else{
+                for(r; r<count; r++){
+                    printf("%s\n", arrayoflists[r]);
+                }
             }
             resetCommand();
-            promptSecond();
+            promptInitial();
         }
     }
 
